bba8b80d6063fdbbf42285fb982aca86e9db1842
John <johnprobussr@gmail.com>
2024-12-29 14:05:31 -0600

    add navbar and style component

--- movieManiac/src/App.jsx
+++ movieManiac/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className="app">
-      <nav className='navbar'>Navbar Content</nav>
+      <Navbar />
 
       <main>Main Content</main>
     </div>
