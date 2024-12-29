961c5c41dd3bd1b2d070bbcb8c6203c0893c531a
John <johnprobussr@gmail.com>
2024-12-29 13:02:03 -0600

    first commit to new React Project

---
+++ movieManiac/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav className='navbar'>Navbar Content</nav>
+
+      <main>Main Content</main>
+    </div>
+  );
+};
+
+export default App;
